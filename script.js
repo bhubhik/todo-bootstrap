c86f3e0e3e7a64025093874e1714b684a8a3ad35
Anubhab Karki <bhubhik@gmail.com>
2022-02-26 16:52:17 +1100

    Todo

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-let count = 0;
-let number = document.querySelector('h4');
+let todo = document.querySelector('input');
+let submit = document.querySelector('.submit-btn');
+let todoContainer = document.querySelector('.todo-container');
 
-let incCounter = () => {
-  count++;
-  number.innerHTML = count;
-};
-let decCounter = () => {
-  count--;
-  number.innerHTML = count;
+let addTodo = () => {
+  if (todo.value) {
+    let makeLi = document.createElement('li');
+    makeLi.classList.add(
+      'bg-primary',
+      'text-center',
+      'text-white',
+      'rounded',
+      'mt-4',
+      'p-2'
+    );
+    makeLi.appendChild(document.createTextNode(todo.value));
+    todoContainer.appendChild(makeLi);
+    todo.value = '';
+
+    makeLi.onclick = function () {
+      this.parentNode.removeChild(this);
+    };
+  } else {
+    alert('List cannot be empty');
+  }
 };
